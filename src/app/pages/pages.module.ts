f3300e24ba3d0bb30beeb91ba79138b39fc97caf
Daniel Nieto <dnietosadurni@gmail.com>
2020-08-12 18:28:12 +0200

    Input, Output y Viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
+
+
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
 // 0 tambien se puede importar RouterModule de @angular/router e importarlo
+import { FormsModule } from '@angular/forms';
+import { ComponentsModule } from '../components/components.module';
 
 
 @NgModule({
@@ -28,7 +33,9 @@ import { AppRoutingModule } from '../app-routing.module';
   imports: [ 
     CommonModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ComponentsModule
   ]
 
 })
